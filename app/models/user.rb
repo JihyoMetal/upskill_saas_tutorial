5a9acfbb983480c8e2ac5d054ce5dc8153fe8a9e
David David <david_doanx@yahoo.com>
2020-04-02 14:24:45 +0000

    Home page ui improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  
+  # Each user belongs to a plan & has 1 profile
   belongs_to :plan
   has_one :profile
   
+  # Whitelist this so we can grab info from the pro form
   attr_accessor :stripe_card_token
-  # If Pro user passes validations (email, passwords, etc.),
-  # then call Stripe and tell Stripe to set up a subscription
-  # upon charging the customer's card.
-  #Stripe responds back with customer data.
-  #Store customer.id as the customer token and save the user.
+  
   def save_with_subscription
+    # We need to check if they pass the devise validation
     if valid?
+      # This is the line where stripe actually charges the customer
+      # and starts the subscription
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      
+      # Stripe responds back with customer data so we store that to the DB
       self.stripe_customer_token = customer.id
       save!
     end
